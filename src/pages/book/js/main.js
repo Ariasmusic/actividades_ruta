635714f59b2309743b7d4f35562ebf484b16b7fe
Ariasmusic <165522639+Ariasmusic@users.noreply.github.com>
2025-07-26 15:42:17 -0500

    Add files via upload

---
+++ src/pages/book/js/main.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const flipbook = document.getElementById("flipbook");
+
+    const pageFlip = new St.PageFlip(flipbook, {
+        width: 900,
+        height: 800,
+        size: "fixed",
+        minWidth: 315,
+        maxWidth: 1000,
+        minHeight: 420,
+        maxHeight: 1350,
+        maxShadowOpacity: 0.5,
+        showCover: true,
+        mobileScrollSupport: false
+    });
+
+    const pages = flipbook.querySelectorAll(".page");
+
+    // Si hay un número impar de páginas internas, agregamos una página en blanco
+    if ((pages.length - 1) % 2 !== 0) {
+        const blankPage = document.createElement("div");
+        blankPage.classList.add("page");
+        blankPage.innerHTML = "<div class='page-content'></div>";
+        flipbook.insertBefore(blankPage, flipbook.lastElementChild); // antes de contraportada
+    }
+
+    pageFlip.loadFromHTML(flipbook.querySelectorAll(".page"));
+
+    document.getElementById("prev-btn").addEventListener("click", () => {
+        pageFlip.flipPrev();
+    });
+
+    document.getElementById("next-btn").addEventListener("click", () => {
+        pageFlip.flipNext();
+    });
+});
