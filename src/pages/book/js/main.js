a15c5c90b904e93471abde1b2157a64ffd095bd5
Ariasmusic <165522639+Ariasmusic@users.noreply.github.com>
2025-08-10 19:13:40 -0500

    Update main.js

--- src/pages/book/js/main.js
+++ src/pages/book/js/main.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const audio = document.getElementById("audio");
 
     const pageFlip = new St.PageFlip(flipbook, {
-        width: 600,
-        height: 500,
+        width: 700,
+        height: 550,
         size: "fixed",
         minWidth: 315,
         maxWidth: 1000,
@@ -77,3 +77,4 @@ document.querySelectorAll(".audio-button").forEach((button) => {
         reproducirAudio(audioId);
     });
 });
+
